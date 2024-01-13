bb3fdd8eca33842034a695005a68c91653765a80
lorenzo santangelo <lorenzo.santangelo1993@gmail.com>
2024-01-13 15:49:35 +0100

    click-dinamic

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ const testo = document.getElementById("testo");
 const btn = document.getElementById("btn")
 let container = document.getElementById("container");
 const testo2 = document.getElementById("testo2");
+let visibile = "hidden";
+let colore = "";
 
 function changeColor() {
-    testo.style.color = "red"
+    if (colore == "") {
+        testo.style.color = "red"
+        colore = "red";
+    } else {
+        testo.style.color = "";
+        colore = "";
+
+
+    }
+
+
 }
 function compare() {
-    testo2.style.visibility = "visible"
+    if (visibile == "hidden") {
+        testo2.style.visibility = "visible"
+        visibile = "visible";
+
+    } else {
+        testo2.style.visibility = "";
+        visibile = "hidden";
+
+    }
+
 }
 btn.addEventListener("click", changeColor)
 btn.addEventListener("click", compare)
