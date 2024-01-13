afe7574cd8183339a1f6a852588e1e34a0d6922f
lorenzo santangelo <lorenzo.santangelo1993@gmail.com>
2024-01-13 15:31:44 +0100

    add-h2

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const testo = document.getElementById("testo");
 const btn = document.getElementById("btn")
 let container = document.getElementById("container");
+const testo2 = document.getElementById("testo2");
 
 function changeColor() {
     testo.style.color = "red"
 }
+function compare() {
+    testo2.style.visibility = "visible"
+}
 btn.addEventListener("click", changeColor)
+btn.addEventListener("click", compare)
