a21a6340323178770f5e5ecfb3aae7a5e73ed15a
lorenzo santangelo <lorenzo.santangelo1993@gmail.com>
2024-01-15 14:32:01 +0100

    add-mini-dinamiclist

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const testo = document.getElementById("testo");
 const btn = document.getElementById("btn")
 let container = document.getElementById("container");
@@ -7,11 +8,6 @@ let colore = "";
 let inputNuovo = document.getElementById("input1")
 let btnInput = document.getElementById("btnInput");
 
-
-
-
-
-
 function changeColor() {
     if (colore == "") {
         testo.style.color = "red"
@@ -19,30 +15,52 @@ function changeColor() {
     } else {
         testo.style.color = "";
         colore = "";
-
-
     }
-
-
 }
 function compare() {
     if (visibile == "hidden") {
         testo2.style.visibility = "visible"
         visibile = "visible";
-
     } else {
         testo2.style.visibility = "";
         visibile = "hidden";
-
     }
-
 }
-
-
 btnInput.addEventListener("click", function () {
     const newTesto = inputNuovo.value;
     testo.textContent = newTesto
+    inputNuovo.value = "";
 });
-
 btn.addEventListener("click", changeColor)
 btn.addEventListener("click", compare)
+
+
+
+let lista = document.getElementById("list");
+let btnList = document.getElementById("btnList");
+let conteggio = document.getElementById("conteggio");
+let conto = 0;
+let btnElimina = document.getElementById("btnElimina");
+
+
+
+function addElement() {
+    conto += 1;
+    const newLi = document.createElement("li");
+    newLi.setAttribute("id", conto);
+    //newLi.textContent = "Elemento " + conto + " Aggiunto";
+    newLi.textContent = `Elemento ${conto} Aggiunto`;
+    lista.appendChild(newLi);
+    conteggio.textContent = "Numero Elementi:" + conto;
+
+}
+
+function remove() {
+    lista.removeChild(lista.lastChild);
+}
+
+
+
+btnElimina.addEventListener("click", remove)
+btnList.addEventListener("click", addElement)
+
